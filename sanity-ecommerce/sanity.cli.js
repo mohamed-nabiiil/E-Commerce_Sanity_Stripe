96a9050abfd96c42a305b35bb0bac4d3848d5994
mohamed-nabiiil <mamadonabil919@gmail.com>
2023-01-03 10:16:08 +0200

    first commit

---
+++ sanity-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '3cc9evm5',
+    dataset: 'production'
+  }
+})
---
+++ sanity-ecommerce/schemas/index.js
@@ -0,0 +1,10 @@
+import product from "./product"
+import banner from "./banner"
+
+export const schemaTypes = [
+  // Document types
+  product,
+  banner, 
+
+  
+]
